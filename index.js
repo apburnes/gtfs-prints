0b5dc2c2b8752a1dc48531422735e3ddde59c69a
Andrew Burnes <apburnes@gmail.com>
2015-05-03 13:20:36 -0600

    Increased line thickness

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ var path = require('path');
 var fs = require('fs');
 var shp2stl = require('shp2stl');
 
-var transFile = path.join(__dirname, 'data', 'sfmta_polys.shp');
-var stlFile = path.join(__dirname, 'stl', 'sfmta.stl');
+var transFile = path.join(__dirname, 'data', 'sfmta_25m_polys.shp');
+var stlFile = path.join(__dirname, 'stl', 'sfmta_25m.stl');
 
 shp2stl.shp2stl(transFile, {
   width: 100,
-  height: 5,
+  height: 3,
   extraBaseHeight: 0,
   extrudeBy: 'thickness',
-  simplification: .5,
+  simplification: .8,
   binary: true,
   cutoutHoles: false,
   verbose: true,
-  extrusionMode: 'straight'
+  extrusionMode: 'smooth'
 }, function(err, stl) {
   if (err) {
     process.exit(1);
